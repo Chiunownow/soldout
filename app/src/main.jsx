b15b7aeb8f985e82c27867407aa3feab32fb20d8
chiunownow <chiunownow@gmail.com>
2025-12-26 01:15:38 +0800

    fix: Remove unused antd-mobile global import
    Removed the import statement for antd-mobile/es/global from main.jsx, as antd-mobile has been uninstalled. This resolves a server error during compilation.

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'antd-mobile/es/global'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
